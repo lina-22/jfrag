f2761d2fe059546930f48e92b3d023ccef4ce404
lina-22 <haquefahinur@gmail.com>
2024-01-26 00:41:27 +0100

    bismillah initial commit

---
+++ jfrag_api/jfrag_api/src/main/java/com/stock/jfrag_api/JfragApiApplication.java
@@ -0,0 +1,15 @@
+package com.stock.jfrag_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class JfragApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfragApiApplication.class, args);
+		System.out.println("Bismillahir Rahmanir Rahim");
+	}
+
+}
---
+++ jfrag_api/jfrag_api/src/test/java/com/stock/jfrag_api/JfragApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.jfrag_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfragApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
