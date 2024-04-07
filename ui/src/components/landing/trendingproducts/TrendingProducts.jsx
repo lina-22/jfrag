4e3bd2c30057acd0bd16a99690c1869e0725d5ac
ships22 <sshipsss@gmail.com>
2024-04-07 18:45:49 +0200

    landing page responsive completed

--- ui/src/components/landing/trendingproducts/TrendingProducts.jsx
+++ ui/src/components/landing/trendingproducts/TrendingProducts.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { PromoProduct } from './promoproduct/PromoProduct'
+import styles from '../trendingproducts/trendingproducts.module.css'
+import { TrendingProduct } from './trendingProduct/TrendingProduct'
 import dramatic from '../../../assests/newProducts/dramatic_desert_1.png'
 import desert from '../../../assests/newProducts/desert_night_1.png'
 import lilium from '../../../assests/newProducts/lilium_sweet_sand_1.png'
 import nuit from '../../../assests/newProducts/nuit_de_noel_1.png'
-import styles from './promoproducts.module.css'
 const products = [
   {
     id: 1,
@@ -22,7 +22,7 @@ const products = [
     description: 'Eau de perfum',
     category: 'Unisex',
     size: '100ml',
-    price: 89,
+    price: 99,
     reduction: 30,
     photo: desert,
   },
@@ -47,24 +47,25 @@ const products = [
     photo: nuit,
   },
 ]
-export const PromoProducts = () => {
+export const TrendingProducts = () => {
   return (
-    <div className={styles.promo_cart_container}>
-      <h3>en promotion</h3>
-      <div className={styles.promo_cart}>
-        <section className={styles.promo_cart_items}>
-          <PromoProduct product={products[0]} />
-          <PromoProduct product={products[1]} />
-          <PromoProduct product={products[2]} />
-          <PromoProduct product={products[3]} />
-          <PromoProduct product={products[0]} />
-          <PromoProduct product={products[1]} />
-          <PromoProduct product={products[2]} />
-          <PromoProduct product={products[3]} />
+    <div className={styles.trend_cart_container}>
+      <div className={styles.trend_cart}>
+        <p className={styles.ltr_a}>A</p>
+        <section className={styles.top_midle}>
+          <TrendingProduct product={products[0]} />
         </section>
-        <div className={styles.link_more}>
-          <a href='google.com'>Et encore plus...</a>
-        </div>
+        <section className={styles.top_right}>
+          <TrendingProduct product={products[1]} />
+        </section>
+        <p className={styles.ltr_la}>LA</p>
+        <section className={styles.bottom_left}>
+          <TrendingProduct product={products[2]} />
+        </section>
+        <section className={styles.bottom_midle}>
+          <TrendingProduct product={products[3]} />
+        </section>
+        <p className={styles.ltr_mode}>MODE</p>
       </div>
     </div>
   )
