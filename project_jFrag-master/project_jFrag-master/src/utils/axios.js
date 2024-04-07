1455f4455b839082f347b3c3b0e0574dadc1489d
ships22 <sshipsss@gmail.com>
2024-04-07 19:11:51 +0200

    UI init, project refactored

--- project_jFrag-master/project_jFrag-master/src/utils/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const axiosInstance = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com/',
-  //   timeout: 1000,
-  //   headers: { 'X-api-key': 'foobar' },
-})
-
-export default axiosInstance
--- project_jFrag-master/project_jFrag-master/src/utils/calculateDiscountPrice.js
+++
@@ -1,5 +0,0 @@
-const getDiscountPrice = (actPrice, discount) => {
-  return (actPrice - actPrice * (discount / 100)).toFixed(2)
-}
-
-export default getDiscountPrice
