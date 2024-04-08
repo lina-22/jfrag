548bd1c7da67c385a0c89fd4d01965d0e71001fa
lina-22 <haquefahinur@gmail.com>
2024-04-08 23:20:53 +0200

    working on sales by category

--- jfrag_admin_ui/src/app/sales-by-category/sales-by-category.component.ts
+++ jfrag_admin_ui/src/app/sales-by-category/sales-by-category.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-sales-by-category',
   templateUrl: './sales-by-category.component.html',
-  styleUrl: './sales-by-category.component.css'
+  styleUrl: './sales-by-category.component.css',
 })
-export class SalesByCategoryComponent {
+export class SalesByCategoryComponent implements OnInit {
+  chart = new Chart({
+    chart: {
+      type: 'pie',
+      height: 325,
+    },
+    title: {
+      text: 'Category wise sales',
+    },
+    xAxis: {
+      categories: ['Male', 'Female', 'Children', 'Ladies'],
+    },
+    yAxis: {
+      title: {
+        text: 'Revenue in %',
+      },
+    },
+    series: [
+      {
+        type: 'pie',
+        data: [
+          {
+            name: 'Parfume',
+            y: 41.0,
+            color: '#044342',
+          },
+        ],
+      },
+    ],
+    credits: {
+      enabled: false,
+    },
+  });
 
+  constructor() {}
+  ngOnInit(): void {}
 }
--- jfrag_admin_ui/src/app/sales-by-month/sales-by-month.component.ts
+++ jfrag_admin_ui/src/app/sales-by-month/sales-by-month.component.ts
@@ -40,9 +40,27 @@ export class SalesByMonthComponent implements OnInit {
       {
         name: 'LÎlÎUM',
         type: 'line',
+        color: '#044342',
         data: [70, 69, 95, 158, 166, 145, 256, 124],
       },
+
+      {
+        name: 'LÎlÎUM-x',
+        type: 'line',
+        color: '#7e0505',
+        data: [70, 9, 95, 158, 66, 14, 56, 124],
+      },
+
+      {
+        name: 'LÎlÎUM-y',
+        type: 'line',
+        color: '#ed9e20',
+        data: [10, 29, 35, 18, 166, 175, 256, 124],
+      },
     ],
+    credits: {
+      enabled: false,
+    },
   });
   constructor() {}
   ngOnInit(): void {}
